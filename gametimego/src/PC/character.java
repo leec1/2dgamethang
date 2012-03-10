efc888c19ef83e907ed2f8f12737c992ff90b406
Ungoth <Agentsnakezero@gmail.com>
2012-03-09 19:21:23 -0500

    coding

--- gametimego/src/PC/character.java
+++ gametimego/src/PC/character.java
@@ -1,5 +1,20 @@
 package PC;
 
-public class character {
+public abstract class character {
+	inventory inv;
+	int str,dex,wis,intl,cha,con;
+	public character(String name){
+	inv=new inventory(str);     
 	
+	
+	
+	
+	
+	}
+	
+	public void useItem(String itemname){
+		inventoryItem tobeused=inv.remove(itemname);
+		
+		
+	}
 }
--- gametimego/src/PC/inventory.java
+++ gametimego/src/PC/inventory.java
@@ -1,5 +1,25 @@
 package PC;
-
+import java.util.*;
 public class inventory {
+ArrayList<inventoryItem> inventory;
+int maxWeight,currentWeight;
+public inventory(int strength){
+	
+	
+	
+	
+}
+
+ public void addItem(inventoryItem item){
+	
+	
+	
+	
+}
+public inventoryItem remove(String itemName){
+	
+	
+	return null;
+}
 
 }
