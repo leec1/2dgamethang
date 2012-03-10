fa4ba9f4bc8d2d1b9c87137274ec4e2d9d1212c4
leec1 <leec1@appstate.edu>
2012-03-09 21:34:02 -0500

    HashMap implementation of Inventory. Needs to standardize itemName when instantiating inventoryItems. Propose: all to lower case. (already doing that in remove.

--- gametimego/src/PC/inventory.java
+++ gametimego/src/PC/inventory.java
@@ -1,71 +1,67 @@
 package PC;
 import java.util.*;
-public class inventory {
-ArrayList<tuple> inventory;
-int maxWeight,currentWeight;
-public inventory(int strength){
-	maxWeight= (int) (68 * Math.pow(Math.E, .1*strength)) ;
-	currentWeight=0;
-	
-	
-}
 
- public void addItem(inventoryItem item){
-	tuple t = new tuple(item);
-	if(currentWeight+item.getItemWeight() <maxWeight){
-    for(int i=0;i<inventory.size();i++)
-    {
-    	if(t.getItem().getName()==inventory.get(i).getItem().getName())
-    	{
-    		int quant = inventory.get(i).getQuantity();
-    		inventory.get(i).setQuantity(quant++);
-    		currentWeight+=inventory.get(i).getItem().getItemWeight();
-    		return;
-    	}
-    	
-    }
-    currentWeight+= t.getItem().getItemWeight();
-	inventory.add(t);
+public class inventory{
+
+	private HashMap<String, tuple> inventory;
+	int maxWeight,currentWeight;
+
+	public inventory(int strength){
+		maxWeight= (int) (68 * Math.pow(Math.E, .1*strength)) ;
+		currentWeight=0;
+
 	}
-	
-}
-public inventoryItem remove(String itemName){
-	inventoryItem item=null;
-	for(int i=0;i<inventory.size();i++)
-	{
-		if(inventory.get(i).getItem().getName().equalsIgnoreCase(itemName)){
-			item=inventory.get(i).getItem();
-			currentWeight-=inventory.get(i).getItem().getItemWeight();
-			if(inventory.get(i).getQuantity()>1){
-				inventory.get(i).setQuantity(inventory.get(i).getQuantity()-1);
-				
-			}
-			else
-				inventory.remove(i);
+
+	public void addItem(inventoryItem item){
+		tuple t = new tuple(item);
+		if(currentWeight+item.getItemWeight() <maxWeight){
+			tuple t = inventory.get(item.getName());
+			if(t != null){
+    				t.setQuantity(t.getQuantity()+1);
+    				currentWeight+=item.getItemWeight();
+    				return;
+			}else{
+				t = new tuple(item);
+				inventory.add(item.getName(), t);
+				currentWeight+= item.getItemWeight();
+			}	
 		}
 	}
-	return item;
-}
-private class tuple{
-	private int quantity;
-	private int getQuantity() {
-		return quantity;
-	}
-	
-	private void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	private inventoryItem item;
-	private tuple(inventoryItem i){
-		item=i;
-		quantity=1;
-	}
-	private inventoryItem getItem() {
+
+	public inventoryItem remove(String itemName)
+		tuple t =inventory.get(itemName.toLowerCase());
+		if(t!=null){
+			inventoryItem item = t.getItem();
+			currentWeight -= item.getItemWeight();
+			if(t.getQuantity()>1){
+				t.setQuantity(t.getQuantity()-1);
+			}else{
+				inventory.remove(itemName.toLowerCase());
+			}
+		}
 		return item;
 	}
-	private void setItem(inventoryItem item) {
-		this.item = item;
-	}
+
+	private class tuple{
+		private int quantity;
+		private int getQuantity() {
+			return quantity;
+		}
+		
+		private void setQuantity(int quantity) {
+			this.quantity = quantity;
+		}
+		private inventoryItem item;
+		private tuple(inventoryItem i){
+			item=i;
+			quantity=1;
+		}
+		private inventoryItem getItem() {
+			return item;
+		}
+		private void setItem(inventoryItem item) {
+			this.item = item;
+		}
 	
-}
+	}
 }
