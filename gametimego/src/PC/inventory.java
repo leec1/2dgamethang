be4f84db4d45696e3e62ad6fb42215c16f913afa
Ungoth <Agentsnakezero@gmail.com>
2012-03-09 20:00:02 -0500

    add and remove added

--- gametimego/src/PC/inventory.java
+++ gametimego/src/PC/inventory.java
@@ -12,21 +12,41 @@ public inventory(int strength){
 
  public void addItem(inventoryItem item){
 	tuple t = new tuple(item);
-    
-	
+    for(int i=0;i<inventory.size();i++)
+    {
+    	if(t.getItem().getName()==inventory.get(i).getItem().getName())
+    	{
+    		int quant = inventory.get(i).getQuantity();
+    		inventory.get(i).setQuantity(quant++);
+    		return;
+    	}
+    	
+    }
+	inventory.add(t);
 	
 	
 }
 public inventoryItem remove(String itemName){
-	
-	
-	return null;
+	inventoryItem item=null;
+	for(int i=0;i<inventory.size();i++)
+	{
+		if(inventory.get(i).getItem().getName().equalsIgnoreCase(itemName)){
+			item=inventory.get(i).getItem();
+			if(inventory.get(i).getQuantity()>1){
+				inventory.get(i).setQuantity(inventory.get(i).getQuantity()-1);
+			}
+			else
+				inventory.remove(i);
+		}
+	}
+	return item;
 }
 private class tuple{
 	private int quantity;
 	private int getQuantity() {
 		return quantity;
 	}
+	
 	private void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
--- gametimego/src/PC/inventoryItem.java
+++ gametimego/src/PC/inventoryItem.java
@@ -1,5 +1,13 @@
 package PC;
 
 public abstract class inventoryItem {
+	String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
